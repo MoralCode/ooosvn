e99b7d5ff4b23ed4aa2313b6572cb0340a73c633
edholness <edholness@2abb7df7-5d29-0410-912f-aa4066f74452>
2007-03-07 14:03:25 +0000

    remove badly structured files

--- listversions.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-mkdir $4/$2/versions/
-
-for version in `svn log -q $3/$2 | grep r | cut -c2- | cut -d' ' -f1`
-do
-svn >> $4/$2/versions/$version.version
-#svn >> $4/$2/versions/log.txt
-done
--- revert.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-cd $4/
-
-rm -rf $4/$2/trunk/
-svn checkout -r$5 $3/$2/
-
-cd $4/$2/trunk/
-zip -rDX $1 * -x *.svn*
